13344887e40de52976d01f1f95a326002c4a7f57
Lucas Oliveira <lucas.oliveira845@gmail.com>
2026-01-10 14:34:03 -0300

    Added Enums for the Expense class

---
+++ Project/src/model/Category.java
@@ -0,0 +1,13 @@
+package model;
+
+public enum Category {
+    FOOD,
+    TRANSPORT,
+    HOUSING,
+    EDUCATION,
+    HEALTH,
+    INVESTMENTS,
+    SHOPPING,
+    ENTERTAINMENT,
+    EMERGENCY
+}
---
+++ Project/src/model/PaymentMethod.java
@@ -0,0 +1,8 @@
+package model;
+
+public enum PaymentMethod {
+    CREDIT,
+    DEBIT,
+    CASH,
+    PIX
+}
