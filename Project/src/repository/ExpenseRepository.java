64c09caf597f65a6ad001bf06d8118b3674da587
Lucas Oliveira <lucas.oliveira845@gmail.com>
2026-01-11 23:34:54 -0300

    Added basic methods at repository layer

--- Project/src/repository/ExpenseRepository.java
+++ Project/src/repository/ExpenseRepository.java
@@ -1,4 +1,24 @@
 package repository;
 
+import model.Category;
+import model.Expense;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class ExpenseRepository {
+    Set<Expense> expenseSet = new HashSet<>();
+
+    public void add(Expense expense){
+        expenseSet.add(expense);
+    }
+
+    public void remove(Expense expense){
+        expenseSet.remove(expense);
+    }
+
+    public List<Expense> findAll(){
+        return List.copyOf(expenseSet);
+    }
 }
