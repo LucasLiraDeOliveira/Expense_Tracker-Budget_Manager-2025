e2a37a0c292544b615058700e7b4c16213278dbc
LucasLiraDeOliveira <lucas.oliveira845@gmail.com>
2026-01-27 21:49:03 -0300

    UI - Adjusting some details at the menus

--- Project/src/ui/ConsoleMenu.java
+++ Project/src/ui/ConsoleMenu.java
@@ -120,22 +120,20 @@ public class ConsoleMenu {
 
 
     private void AddExpense(){
-        System.out.println("How much was the Expense:");
+        System.out.println("\nHow much was the Expense:");
         BigDecimal amount = new BigDecimal(scanner.nextLine());
 
-        System.out.println("Description of the Expense:");
+        System.out.println("\nDescription of the Expense:");
         String description = scanner.nextLine().toLowerCase();
 
 
         // Category attribute part:
-        scanner.nextLine(); // clear leftover newline
-        System.out.println("What's the category of this Expense?");
+        System.out.println("\nWhat's the category of this Expense?");
         Category category = pickCategory();
 
 
         // Payment Method attribute part:
-        scanner.nextLine(); // clear leftover newline
-        System.out.println("How did you pay?");
+        System.out.println("\nHow did you pay?");
         PaymentMethod paymentMethod = pickPaymentMethod();
 
 
@@ -169,8 +167,8 @@ public class ConsoleMenu {
         Category category = null;
 
         while (category == null) {
-            System.out.print("The possibilities are:\nFOOD,\nTRANSPORT,\nHOUSING,\nEDUCATION,\nHEALTH,\nINVESTMENTS," +
-                    "\nSHOPPING,\nENTERTAINMENT,\nEMERGENCY");
+            System.out.println("The possibilities are:\nFOOD,\nTRANSPORT,\nHOUSING,\nEDUCATION," +
+                    "\nHEALTH,\nINVESTMENTS,\nSHOPPING,\nENTERTAINMENT,\nEMERGENCY");
             String input = scanner.nextLine().trim();
 
             if (input.isEmpty()) continue;
@@ -189,7 +187,7 @@ public class ConsoleMenu {
         PaymentMethod paymentMethod = null;
 
         while (paymentMethod == null) {
-            System.out.print("The possibilities are:\nCREDIT,\nDEBIT,\nCASH,\nPIX");
+            System.out.println("The possibilities are:\nCREDIT,\nDEBIT,\nCASH,\nPIX");
             String input = scanner.nextLine().trim();
 
             if (input.isEmpty()) continue;
@@ -205,7 +203,7 @@ public class ConsoleMenu {
 
 
     private void RemoveExpense(){
-        System.out.println("What is the ID of the expense that you want to remove?");
+        System.out.println("\n\nWhat is the ID of the expense that you want to remove?");
         long idNumber = scanner.nextLong();
 
         boolean removed = service.removeExpenseById(idNumber);
@@ -224,7 +222,7 @@ public class ConsoleMenu {
         while (running){
             int year = validationOfYear();
 
-            System.out.println("Do you want to list your expenses by what:\n1- List by Category\n2 - List By time\n3 " +
+            System.out.println("\nDo you want to list your expenses by what:\n1- List by Category\n2 - List By time\n3 " +
                     "- List by amount range\n4 - List by payment method\n5 - List all expenses of a year\n0 - Back to" +
                     " main menu");
             int option = readOption();
@@ -247,13 +245,14 @@ public class ConsoleMenu {
         int year = 0;
 
         while (!validYear){
-            System.out.println("Do you want to see the expenses of which year?");
+            System.out.println("\nDo you want to list the expenses of which year?");
             year = readOption();
 
             if (year > 2000 && year <= 2100) {
                 validYear = true;
             } else {
-                System.out.println("We're working with expenses at 21st century. Please put a valid value!");
+                System.out.println("\nWe're working with expenses at 21st century. Please put a " +
+                        "valid value!");
             }
         }
 
@@ -263,7 +262,7 @@ public class ConsoleMenu {
 
     //Listing sub-menus methods:
     public void ListByCategoryMenu(int year){
-        System.out.println("Do you want to list your expenses by which Category?");
+        System.out.println("\nDo you want to list your expenses by which Category?");
         Category category = pickCategory();
 
          List<Expense> expenseList = service.ListByCategory(year, category);
@@ -271,7 +270,7 @@ public class ConsoleMenu {
     }
 
     public void ListByTimeMenu(int year){
-        System.out.println("Do you want to list the expenses of:\n1 - the whole year\n2 - an semester\n3 - An" +
+        System.out.println("\nDo you want to list the expenses of:\n1 - the whole year\n2 - an semester\n3 - An" +
                 " specific month");
         int dataChoice = readOption();
 
@@ -281,14 +280,14 @@ public class ConsoleMenu {
                 service.ListByTime(1, year, 0).forEach(System.out::println);
                 break;
             case 2:
-                System.out.println("Which semester of " + year + " you want to choose?  (1  OR  2)");
+                System.out.println("\nWhich semester of " + year + " you want to choose?  (1  OR  2)");
                 int semester = readOption();
 
                 System.out.println("\nAll expenses of semester " + semester + " of " + year + ":");
                 service.ListByTime(2, year, semester).forEach(System.out::println);
                 break;
             case 3:
-                System.out.println("Which month of " + year + " you want to choose?  (1  TO  12)");
+                System.out.println("\nWhich month of " + year + " you want to choose?  (1  TO  12)");
                 int month = readOption();
 
                 System.out.println("\nAll expenses of month " + month + " of " + year + ":");
@@ -300,25 +299,25 @@ public class ConsoleMenu {
     }
 
     public void ListByAmountMenu(int year){
-        System.out.println("What's the lower value of the amount range you want to list:");
+        System.out.println("\nWhat's the lower value of the amount range you want to list:");
         BigDecimal lowerValue = new BigDecimal(scanner.nextLine());
         System.out.println("What's the upper value of the amount range you want to list:");
         BigDecimal upperValue = new BigDecimal(scanner.nextLine());
 
-        System.out.println("All expenses between R$" + lowerValue + " and R$" + upperValue + " are:");
+        System.out.println("\nAll expenses between R$" + lowerValue + " and R$" + upperValue + " are:");
         service.ListByAmount(year, lowerValue, upperValue).forEach(System.out::println);
     }
 
     public void ListByPaymentMethodMenu(int year){
-        System.out.println("Do you want a list of expenses based on which Payment method:");
+        System.out.println("\nDo you want a list of expenses based on which Payment method:");
         PaymentMethod paymentMethod = pickPaymentMethod();
 
-        System.out.println("This are all the expenses that with " + paymentMethod + " in " + year + " :");
+        System.out.println("\nThis are all the expenses that with " + paymentMethod + " in " + year + " :");
         service.ListByPaymentMethod(year, paymentMethod).forEach(System.out::println);
     }
 
     public void ListAllExpensesMenu(int year){
-        System.out.println("This are all the expenses of " + year + " :");
+        System.out.println("\nThis are all the expenses of " + year + " :");
         service.ListAllExpenses(year).forEach(System.out::println);
     }
 }
